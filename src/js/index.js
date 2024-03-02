0288c519cc217f43a20d52c50cc74e8424375796
YarNeplotkin <yaroslavse2004@yandex.ru>
2024-03-02 12:02:31 +0300

    make a canvas

---
+++ src/js/index.js
@@ -0,0 +1,7 @@
+const canvas = document.getElementById("canvas")
+const ctx = canvas.getContext("2d")
+
+ctx.fillStyle = "#00ff00"
+ctx.fillRect(50,50,100,100)
+// ctx.fillStyle = "#00ff00"; // Задаем цвет заливки
+// ctx.fillRect(50, 50, 100, 100); // Рисуем прямоугольник с координатами (50, 50) и размерами 100x100 пикселей
